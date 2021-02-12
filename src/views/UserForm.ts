8fce355f54d7259d920e36defc14e216a8bd4ec6
rafael.fumegalli <rafaelxxc@gmail.com>
2021-02-12 15:11:59 -0300

    Class #191

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -4,11 +4,16 @@ export class UserForm {
   eventsMap():{ [key: string]: () => void } {
     return {
       'click:button': this.onButtonClick,
+      'mouseenter:h1': this.onMouseEnter,
     };
   }
 
   onButtonClick(): void {
     console.log('Hi there');
+  }
+
+  onMouseEnter(): void {
+    console.log('Hi mouse');
   }
 
   template(): string {
@@ -21,10 +26,24 @@ export class UserForm {
     `;
   }
 
+  bindEvents(fragment: DocumentFragment): void {
+    const eventsMap = this.eventsMap();
+
+    for (const eventKey in eventsMap) {
+      const [eventName, selector] = eventKey.split(':');
+
+      fragment.querySelectorAll(selector).forEach((element) => {
+        element.addEventListener(eventName, eventsMap[eventKey]);
+      });
+    }
+  }
+
   render(): void {
     const templateElement = document.createElement('template');
 
     templateElement.innerHTML = this.template();
+
+    this.bindEvents(templateElement.content);
 
     this.parent.append(templateElement.content);
   }
