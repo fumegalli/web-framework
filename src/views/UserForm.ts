ee251b653c213907263116eb01494ba18111b4dc
rafael.fumegalli <rafaelxxc@gmail.com>
2021-02-18 13:36:41 -0300

    Class #201: Saving data from a view

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -5,8 +5,13 @@ export class UserForm extends View<User, UserProps> {
   eventsMap():{ [key: string]: () => void } {
     return {
       'click:.set-age': this.onSetAgeClick,
-      'click:.set-name': this.onSetNameClick
+      'click:.set-name': this.onSetNameClick,
+      'click:.save-model': this.onSaveClick,
     };
+  }
+
+  onSaveClick = (): void => {
+    this.model.save();
   }
 
   onSetAgeClick = (): void => {
@@ -26,12 +31,10 @@ export class UserForm extends View<User, UserProps> {
   template(): string {
     return `
       <div>
-        <h1> User Form <\h1>
-        <div> Name: ${this.model.get('name')} </div>
-        <div> Age: ${this.model.get('age')} </div>
-        <input />
+        <input placeholder="${this.model.get('name')}"/>
         <button class="set-name"> Set Name </button>
         <button class="set-age"> Set Random Age </button>
+        <button class="save-model"> Save </button>
       </div>
     `;
   }
